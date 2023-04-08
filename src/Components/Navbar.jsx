ee6196d17589f38f1a7df5d556aaab043fcef9cf
faithyyyy <faithfortune6@gmail.com>
2023-04-08 13:22:33 +0100

    gave an active styling to the mobile nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,70 +1,93 @@
-import Home from "../Svgs/HomeSvg.jsx";
-import NavBudgetSvg from "../Svgs/NavBudgetSvg.jsx";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-white rounded h-14 fixed bottom-10 left-0 nav flex justify-around items-center lg:hidden z-10">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6  text-gray-400 navlink"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
+      <NavLink
+        to="/"
+        className="mobileNav"
+        // className="flex items-center gap-5 cursor-pointer sideNav py-3 pl-2 rounded mb-5"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-        ></path>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-gray-400 navlink"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="2"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6  text-gray-400 navlink"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          ></path>
+        </svg>
+      </NavLink>
+      <NavLink
+        to="/budget"
+        className="mobileNav"
+        // className="flex items-center gap-5 cursor-pointer sideNav py-3 pl-2 rounded mb-5"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-        ></path>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-gray-400 navlink"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="2"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-gray-400 navlink"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+          ></path>
+        </svg>
+      </NavLink>
+      <NavLink
+        to="/expense"
+        className="mobileNav"
+        // className="flex items-center gap-5 cursor-pointer sideNav py-3 pl-2 rounded mb-5"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        ></path>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6 text-gray-400 navlink"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="2"
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-gray-400 navlink"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+      </NavLink>
+      <NavLink
+        to="/history"
+        className="mobileNav"
+        // className="flex items-center gap-5 cursor-pointer sideNav py-3 pl-2 rounded"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-        ></path>
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-        ></path>
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-gray-400 navlink"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
+          ></path>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
+          ></path>
+        </svg>
+      </NavLink>
     </nav>
   );
 }
--- src/Pages/Expense.jsx
+++ src/Pages/Expense.jsx
@@ -1,5 +1,5 @@
 import Header from "../Components/Header";
-import BudgetCard from "../Components/BudgetCard";
+import ExpenseCard from "../Components/ExpenseCard";
 import { FaGraduationCap } from "react-icons/fa";
 import { GiHotMeal } from "react-icons/gi";
 import { IoAirplaneSharp } from "react-icons/io5";
@@ -34,46 +34,46 @@ function Budgets() {
         <div className="bg-white p-12">
           <h3 className="text-[#6c7983] text-center mb-5">Choose a Category</h3>
           <div className="grided">
-            <BudgetCard roles="Education">
-              <FaGraduationCap className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Food">
-              <GiHotMeal className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Travel">
-              <IoAirplaneSharp className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Clothes">
-              <IoShirtOutline className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Health">
-              <RiStethoscopeLine className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Transport">
-              <AiOutlineCar className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Internet">
-              <MdRouter className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Internet">
-              <MdRouter className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Shopping">
-              <BsCart4 className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Rent">
-              <BsBuildingsFill className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
-            <BudgetCard roles="Rent">
-              <BsLightningChargeFill className="text-[#ff7461] text-2xl" />
-            </BudgetCard>
+            <ExpenseCard roles="Education">
+              <FaGraduationCap className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Food">
+              <GiHotMeal className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Travel">
+              <IoAirplaneSharp className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Clothes">
+              <IoShirtOutline className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Health">
+              <RiStethoscopeLine className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Transport">
+              <AiOutlineCar className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Internet">
+              <MdRouter className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Internet">
+              <MdRouter className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Shopping">
+              <BsCart4 className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Rent">
+              <BsBuildingsFill className="text-white text-2xl" />
+            </ExpenseCard>
+            <ExpenseCard roles="Rent">
+              <BsLightningChargeFill className="text-white text-2xl" />
+            </ExpenseCard>
           </div>
         </div>
         <div className="bg-white px-8 pt-8 pb-3 mt-8 relative">
           <h3 className="text-black text-center mb-5">Enter Amount</h3>
           <div className="relative  w-[80%] max-w-[200px] mx-auto">
             <input
-              className="border-b-4 bg-[#ffede9] text-[#6c7983] rounded border-[#ff7461] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
+              className="border-b-4 bg-[#7788f479] text-[#6c7983] rounded border-[#7788f4] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
               placeholder="Enter Budget"
               value={numValue}
               onChange={(e) => changeValue(e)}
@@ -83,7 +83,7 @@ function Budgets() {
             </span>
           </div>
           <div className="flex gap-7   -bottom-8 items-center justify-center relative">
-            <div className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#7788f4]">
+            <div className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#ff7461]">
               <HiOutlineXMark className="text-white text-[29px]" />
             </div>
             {/* <MdCancel className="text-[#7788f4] text-4xl cursor-pointer self-center" /> */}
