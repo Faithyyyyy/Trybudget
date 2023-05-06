51771997d3757ec41c3efe290851e0f39fd67793
faithyyyy <faithfortune6@gmail.com>
2023-05-06 15:52:47 +0100

    changed my progressbar, added my total budget and expense to homepage

--- src/Components/ExpressProgressbar.jsx
+++ src/Components/ExpressProgressbar.jsx
@@ -1,21 +1,34 @@
+import { useSelector } from "react-redux";
 import { PercentageConversion } from "../Utils/PercentageConversion";
-
 function Expressbar() {
-  const convertToPercent = PercentageConversion(50, 60);
+  let r = 1000;
+  let a = 200;
+  const { totalBudgetAmount } = useSelector((store) => {
+    return store.budget;
+  });
+  const { totalExpenseAmount } = useSelector((store) => {
+    return store.expense;
+  });
+  const convertToPercent = PercentageConversion(
+    totalExpenseAmount,
+    totalBudgetAmount
+  );
+  // if (totalExpenseAmount > totalBudgetAmount) {
+  //   totalBudgetAmount = totalExpenseAmount;
+  // }
   return (
     <section className="bg-white  rounded py-10 px-10 w-full mt-10">
       <p className="font-light text-sm text-gray-400">Expenses so far</p>
       <div className="flex justify-between my-2">
-        <p className="font-light text-md">2000</p>
-        <p className="font-light text-md">4000</p>
+        <p className="font-light text-md">${totalExpenseAmount}</p>
+        <p className="font-light text-md">${totalBudgetAmount}</p>
       </div>
-      <div className="w-full h-[10px] rounded-lg bg-[#ff7461] bg-opacity-10">
-        <div
-          className={`h-full rounded-lg bg-[#ff7461] expenseBar `}
-          style={{ width: `${convertToPercent}%` }}
-        ></div>
-      </div>
-      <p></p>
+      <label for="file"></label>
+      <progress
+        id="file"
+        max={totalBudgetAmount}
+        value={totalExpenseAmount}
+      ></progress>
     </section>
   );
 }
--- src/Components/MobileExpressbar.jsx
+++ src/Components/MobileExpressbar.jsx
@@ -1,20 +1,33 @@
+import { useSelector } from "react-redux";
 import { PercentageConversion } from "../Utils/PercentageConversion";
 
 function MobileExpressbar() {
-  const convertToPercent = PercentageConversion(50, 60);
+  const { totalBudgetAmount } = useSelector((store) => {
+    return store.budget;
+  });
+  const { totalExpenseAmount } = useSelector((store) => {
+    return store.expense;
+  });
+  const convertToPercent = PercentageConversion(
+    totalExpenseAmount,
+    totalBudgetAmount
+  );
+  // if (totalExpenseAmount > totalBudgetAmount) {
+  //   totalBudgetAmount = totalExpenseAmount;
+  // }
   return (
     <section className="bg-white  lg:hidden rounded py-10 px-10  mt-10 mx-5">
       <p className="font-light text-sm text-gray-400">Expenses so far</p>
       <div className="flex justify-between my-2">
-        <p className="font-light text-md">2000</p>
-        <p className="font-light text-md">4000</p>
+        <p className="font-light text-md">${totalExpenseAmount}</p>
+        <p className="font-light text-md">${totalBudgetAmount}</p>
       </div>
-      <div className="w-full h-[10px] rounded-lg bg-[#ff7461] bg-opacity-10">
-        <div
-          className={`h-full rounded-lg bg-[#ff7461] expenseBar `}
-          style={{ width: `${convertToPercent}%` }}
-        ></div>
-      </div>
+      <label for="file"></label>
+      <progress
+        id="file"
+        max={totalBudgetAmount}
+        value={totalExpenseAmount}
+      ></progress>
       <p></p>
     </section>
   );
