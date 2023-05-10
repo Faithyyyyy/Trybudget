d2b5c6e644ae6fa1f3d87ccc092de373bcda49b9
faithyyyy <faithfortune6@gmail.com>
2023-05-10 13:55:47 +0100

    Adjusted screen sizes

--- src/Pages/Budgets.jsx
+++ src/Pages/Budgets.jsx
@@ -122,134 +122,139 @@ function Budgets() {
   };
 
   return (
-    <section className="bg-[#ffede9] min-h-screen font-poppins">
-      <div className=" lg:ml-[320px] px-5">
-        <Header />
-
-        <h2 className="underline font-bold text-xl lg:text-2xl text-center mt-8 mb-5">
-          Create Your Budgets
-        </h2>
-        <div className="bg-white p-6 md:p-10" data-aos="fade-out">
-          <h3 className="text-[#6c7983] text-center mb-5">Choose a Category</h3>
-          <div className="grided">
-            {data.map((d) => {
-              return (
-                <div
-                  onClick={() => {
-                    handleclick(d.id);
-                    dispatch(setCurrentCategory(d.role));
-                    dispatch(setcategoryID(d.id));
-                  }}
-                  key={d.id}
-                  className={`bg-[#ffede9]  border-2 transition-opacity opacity-100 ${
-                    selectedBox === d.id ? "  border-[#ff7461]" : "border-white"
-                  }  inline-block justify-center items-center py-3 lg:py-6 rounded relative cursor-pointer`}
-                >
-                  <div className="bg-[#f9e0d9] p-1 rounded-full self-center w-8 mx-auto mb-1">
-                    <div className="text-[#ff7461] text-2xl"> {d.icon}</div>
-                  </div>
-                  <p className="font-light text-xs text-center">{d.role}</p>
-                  {selectedBox === d.id && (
-                    <div className="absolute w-4 h-4 bg-[#ff7461] flex place-content-center items-center rounded-full -right-2 -top-2">
-                      <GrFormCheckmark />
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-          className="bg-white px-8 pt-8 pb-3 mt-8 relative"
-        >
-          <h3 className="text-black text-center mb-5">Enter Amount</h3>
-          <div className="relative  w-[80%] max-w-[200px] mx-auto">
-            <input
-              className="border-b-4 bg-[#ffede9] text-[#6c7983] rounded border-[#ff7461] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
-              placeholder="Enter Budget"
-              value={mainBudget}
-              type="number"
-              id="mainBudget"
-              onChange={(e) => {
-                dispatch(setMainBudget(Number(e.target.value)));
-              }}
-            />
-            <span className="text-[#6c7983] font-light absolute top-2 left-5">
-              $
-            </span>
-          </div>
-          <div className="flex gap-7   -bottom-8 items-center justify-center relative">
-            <div
-              onClick={deselectCategory}
-              className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#7788f4]"
-            >
-              <HiOutlineXMark className="text-white text-[29px]" />
-            </div>
-            <button
-              disabled={disabled}
-              className={`bg-[#ff7461] disabled:bg-[#a7a7a7] uppercase px-5 py-2 text-white rounded-2xl text-sm font-light tracking-wider outline-none`}
-            >
-              create
-            </button>
-          </div>
-        </form>
-        <div className="mt-14 pb-24">
-          <div className="flex justify-between">
-            <h3 className="font-bold text-[#6c7983]">
-              ({budgetList?.length}) Budgets
+    <section className="bg-[#ffede9] min-h-screen font-poppins  border-gray-800">
+      <div className=" max-w-[1420px] largeScreenCentered">
+        <div className=" lg:ml-[320px] px-5  max-w-7xl largeScreenCentered">
+          <Header />
+          <h2 className="underline font-bold text-xl lg:text-2xl text-center mt-8 mb-5">
+            Create Your Budgets
+          </h2>
+          <div className="bg-white p-6 md:p-10" data-aos="fade-out">
+            <h3 className="text-[#6c7983] text-center mb-5">
+              Choose a Category
             </h3>
-            {budgetList.length > 0 && (
-              <button
-                className="bg-gradient-to-tl from-indigo-300 to-red-400 text-black text-xs py-2 px-5 rounded-md"
-                onClick={() => dispatch(clearBudgetlist())}
-              >
-                Clear All
-              </button>
-            )}
-          </div>
-          <div className=" md:grid gap-6 grid-cols-2">
-            {budgetList?.map((budget) => {
-              return (
-                <div
-                  key={budget.id}
-                  className="bg-white flex my-3 md:mb-0 justify-between items-center py-2 px-3 rounded border-l-4 border-[#ff7461]"
-                >
-                  <div className="flex gap-3 items-center">
+            <div className="grided">
+              {data.map((d) => {
+                return (
+                  <div
+                    onClick={() => {
+                      handleclick(d.id);
+                      dispatch(setCurrentCategory(d.role));
+                      dispatch(setcategoryID(d.id));
+                    }}
+                    key={d.id}
+                    className={`bg-[#ffede9]  border-2 transition-opacity opacity-100 ${
+                      selectedBox === d.id
+                        ? "  border-[#ff7461]"
+                        : "border-white"
+                    }  inline-block justify-center items-center py-3 lg:py-6 rounded relative cursor-pointer`}
+                  >
                     <div className="bg-[#f9e0d9] p-1 rounded-full self-center w-8 mx-auto mb-1">
-                      <div className="text-[#ff7461] text-xl ">
-                        {" "}
-                        {data[budget.id - 1].icon}
+                      <div className="text-[#ff7461] text-2xl"> {d.icon}</div>
+                    </div>
+                    <p className="font-light text-xs text-center">{d.role}</p>
+                    {selectedBox === d.id && (
+                      <div className="absolute w-4 h-4 bg-[#ff7461] flex place-content-center items-center rounded-full -right-2 -top-2">
+                        <GrFormCheckmark />
                       </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+            className="bg-white px-8 pt-8 pb-3 mt-8 relative"
+          >
+            <h3 className="text-black text-center mb-5">Enter Amount</h3>
+            <div className="relative  w-[80%] max-w-[200px] mx-auto">
+              <input
+                className="border-b-4 bg-[#ffede9] text-[#6c7983] rounded border-[#ff7461] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
+                placeholder="Enter Budget"
+                value={mainBudget}
+                type="number"
+                id="mainBudget"
+                onChange={(e) => {
+                  dispatch(setMainBudget(Number(e.target.value)));
+                }}
+              />
+              <span className="text-[#6c7983] font-light absolute top-2 left-5">
+                $
+              </span>
+            </div>
+            <div className="flex gap-7   -bottom-8 items-center justify-center relative">
+              <div
+                onClick={deselectCategory}
+                className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#7788f4]"
+              >
+                <HiOutlineXMark className="text-white text-[29px]" />
+              </div>
+              <button
+                disabled={disabled}
+                className={`bg-[#ff7461] disabled:bg-[#a7a7a7] uppercase px-5 py-2 text-white rounded-2xl text-sm font-light tracking-wider outline-none`}
+              >
+                create
+              </button>
+            </div>
+          </form>
+          <div className="mt-14 pb-24">
+            <div className="flex justify-between">
+              <h3 className="font-bold text-[#6c7983]">
+                ({budgetList?.length}) Budgets
+              </h3>
+              {budgetList.length > 0 && (
+                <button
+                  className="bg-gradient-to-tl from-indigo-300 to-red-400 text-black text-xs py-2 px-5 rounded-md"
+                  onClick={() => dispatch(clearBudgetlist())}
+                >
+                  Clear All
+                </button>
+              )}
+            </div>
+            <div className=" md:grid gap-6 grid-cols-2">
+              {budgetList?.map((budget) => {
+                return (
+                  <div
+                    key={budget.id}
+                    className="bg-white flex my-3 md:mb-0 justify-between items-center py-2 px-3 rounded border-l-4 border-[#ff7461]"
+                  >
+                    <div className="flex gap-3 items-center">
+                      <div className="bg-[#f9e0d9] p-1 rounded-full self-center w-8 mx-auto mb-1">
+                        <div className="text-[#ff7461] text-xl ">
+                          {" "}
+                          {data[budget.id - 1].icon}
+                        </div>
+                      </div>
+                      <p className="font-light text-xs text-center">
+                        {budget.name}
+                      </p>
                     </div>
                     <p className="font-light text-xs text-center">
-                      {budget.name}
+                      ${budget.amount}
                     </p>
+                    <div className="flex gap-2">
+                      <HiOutlinePencil
+                        className="text-[#a7a7a7] cursor-pointer"
+                        onClick={() => {
+                          handleclick(budget.id);
+                          dispatch(setMainBudget(budget.amount));
+                          dispatch(setCurrentCategory(budget.name));
+                          dispatch(setIsEditing(true));
+                          dispatch(setEditID(budget.id));
+                        }}
+                      />
+                      <RiDeleteBin5Line
+                        className="text-[#ff7461] cursor-pointer"
+                        onClick={() => dispatch(removeBudgetItem(budget.id))}
+                      />
+                    </div>
                   </div>
-                  <p className="font-light text-xs text-center">
-                    ${budget.amount}
-                  </p>
-                  <div className="flex gap-2">
-                    <HiOutlinePencil
-                      className="text-[#a7a7a7] cursor-pointer"
-                      onClick={() => {
-                        handleclick(budget.id);
-                        dispatch(setMainBudget(budget.amount));
-                        dispatch(setCurrentCategory(budget.name));
-                        dispatch(setIsEditing(true));
-                        dispatch(setEditID(budget.id));
-                      }}
-                    />
-                    <RiDeleteBin5Line
-                      className="text-[#ff7461] cursor-pointer"
-                      onClick={() => dispatch(removeBudgetItem(budget.id))}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/Expense.jsx
+++ src/Pages/Expense.jsx
@@ -145,131 +145,136 @@ function Expense() {
   console.log(expenseList);
   return (
     <section className="bg-[#7788f479] min-h-screen font-poppins">
-      <div className=" lg:ml-[320px] px-5" data-aos="fade-out">
-        <Header />
-
-        <h2 className="underline font-bold text-xl lg:text-2xl text-center mt-8 mb-5">
-          Create Your Expenses
-        </h2>
-        <div className="bg-white p-12">
-          <h3 className="text-[#6c7983] text-center mb-5">Choose a Category</h3>
-          <div className="grided">
-            {data.map((d) => {
-              return (
-                <div
-                  onClick={() => {
-                    checkSelectedExpense(d.id, d.role);
-                  }}
-                  key={d.id}
-                  className={`bg-[#7788f479] border-2 transition-opacity opacity-100 cursor-pointer ${
-                    selectedBox === d.id ? " border-[#7788f4]" : "border-white"
-                  } inline-block justify-center items-center py-3 lg:py-6 rounded relative`}
-                >
-                  <div className="bg-[#7788f4] p-1 rounded-full self-center w-8 mx-auto mb-1">
-                    <div className="text-white text-2xl"> {d.icon}</div>
-                  </div>
-                  <p className="font-light text-xs text-center">{d.role}</p>
-                  {selectedBox === d.id && (
-                    <div className="absolute w-4 h-4 bg-[#7788f4] flex place-content-center items-center rounded-full -right-2 -top-2">
-                      <GrFormCheckmark />
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-          className="bg-white px-8 pt-8 pb-3 mt-8 relative"
-        >
-          <h3 className="text-black text-center mb-5">Enter Amount</h3>
-          <div className="relative  w-[80%] max-w-[200px] mx-auto">
-            <input
-              className="border-b-4 bg-[#7788f479] text-[#6c7983] rounded border-[#7788f4] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
-              placeholder="Enter Budget"
-              value={mainExpense}
-              type="number"
-              id="mainExpense"
-              onChange={(e) => {
-                dispatch(setMainExpense(Number(e.target.value)));
-              }}
-            />
-            <span className="text-[#6c7983] font-light absolute top-2 left-5">
-              $
-            </span>
-          </div>
-          <div className="flex gap-7   -bottom-8 items-center justify-center relative">
-            <div
-              onClick={deselectCategory}
-              className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#ff7461]"
-            >
-              <HiOutlineXMark className="text-white text-[29px]" />
-            </div>
-            <button
-              disabled={disabled}
-              className=" bg-[#7788f4] disabled:bg-[#a7a7a7] uppercase px-5 py-2 text-white rounded-2xl text-sm font-light tracking-wider"
-            >
-              create
-            </button>
-          </div>
-        </form>
-        <div className="mt-14 pb-24">
-          <div className="flex justify-between">
-            <h3 className="font-bold text-[#6c7983]">
-              ({expenseList?.length}) Budgets
+      <div className=" max-w-[1420px] largeScreenCentered">
+        <div className=" lg:ml-[320px] px-5" data-aos="fade-out">
+          <Header />
+          <h2 className="underline font-bold text-xl lg:text-2xl text-center mt-8 mb-5">
+            Create Your Expenses
+          </h2>
+          <div className="bg-white p-12">
+            <h3 className="text-[#6c7983] text-center mb-5">
+              Choose a Category
             </h3>
-            {expenseList?.length > 0 && (
-              <button
-                className="bg-gradient-to-tl from-indigo-300 to-red-400 text-black text-xs py-2 px-5 rounded-md"
-                onClick={() => dispatch(clearExpenselist())}
-              >
-                Clear All
-              </button>
-            )}
-          </div>
-          <div className="my-3  md:grid gap-6 grid-cols-2">
-            {expenseList?.map((expense) => {
-              return (
-                <div
-                  key={expense.id}
-                  className="bg-white flex my-3 md:mb-0 justify-between items-center py-2 px-3 rounded border-l-4 border-[#7788f4]"
-                >
-                  <div className="flex gap-3 items-center">
-                    <div className="bg-[#c3c9f4]  flex justify-center items-center rounded-full self-center w-6 h-6 mx-auto mb-1">
-                      <div className="text-[#7788f4] text-lg ">
-                        {" "}
-                        {data[expense.id - 1].icon}
+            <div className="grided">
+              {data.map((d) => {
+                return (
+                  <div
+                    onClick={() => {
+                      checkSelectedExpense(d.id, d.role);
+                    }}
+                    key={d.id}
+                    className={`bg-[#7788f479] border-2 transition-opacity opacity-100 cursor-pointer ${
+                      selectedBox === d.id
+                        ? " border-[#7788f4]"
+                        : "border-white"
+                    } inline-block justify-center items-center py-3 lg:py-6 rounded relative`}
+                  >
+                    <div className="bg-[#7788f4] p-1 rounded-full self-center w-8 mx-auto mb-1">
+                      <div className="text-white text-2xl"> {d.icon}</div>
+                    </div>
+                    <p className="font-light text-xs text-center">{d.role}</p>
+                    {selectedBox === d.id && (
+                      <div className="absolute w-4 h-4 bg-[#7788f4] flex place-content-center items-center rounded-full -right-2 -top-2">
+                        <GrFormCheckmark />
                       </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e);
+            }}
+            className="bg-white px-8 pt-8 pb-3 mt-8 relative"
+          >
+            <h3 className="text-black text-center mb-5">Enter Amount</h3>
+            <div className="relative  w-[80%] max-w-[200px] mx-auto">
+              <input
+                className="border-b-4 bg-[#7788f479] text-[#6c7983] rounded border-[#7788f4] w-full py-2  flex justify-center outline-none pl-10 pr-5 text-center placeholder:text-[#6c7983] font-poppins font-light text-sm placeholder:text-xs"
+                placeholder="Enter Budget"
+                value={mainExpense}
+                type="number"
+                id="mainExpense"
+                onChange={(e) => {
+                  dispatch(setMainExpense(Number(e.target.value)));
+                }}
+              />
+              <span className="text-[#6c7983] font-light absolute top-2 left-5">
+                $
+              </span>
+            </div>
+            <div className="flex gap-7   -bottom-8 items-center justify-center relative">
+              <div
+                onClick={deselectCategory}
+                className="w-9 flex cursor-pointer justify-center items-center h-9 rounded-full bg-[#ff7461]"
+              >
+                <HiOutlineXMark className="text-white text-[29px]" />
+              </div>
+              <button
+                disabled={disabled}
+                className=" bg-[#7788f4] disabled:bg-[#a7a7a7] uppercase px-5 py-2 text-white rounded-2xl text-sm font-light tracking-wider"
+              >
+                create
+              </button>
+            </div>
+          </form>
+          <div className="mt-14 pb-24">
+            <div className="flex justify-between">
+              <h3 className="font-bold text-[#6c7983]">
+                ({expenseList?.length}) Budgets
+              </h3>
+              {expenseList?.length > 0 && (
+                <button
+                  className="bg-gradient-to-tl from-indigo-300 to-red-400 text-black text-xs py-2 px-5 rounded-md"
+                  onClick={() => dispatch(clearExpenselist())}
+                >
+                  Clear All
+                </button>
+              )}
+            </div>
+            <div className="my-3  md:grid gap-6 grid-cols-2">
+              {expenseList?.map((expense) => {
+                return (
+                  <div
+                    key={expense.id}
+                    className="bg-white flex my-3 md:mb-0 justify-between items-center py-2 px-3 rounded border-l-4 border-[#7788f4]"
+                  >
+                    <div className="flex gap-3 items-center">
+                      <div className="bg-[#c3c9f4]  flex justify-center items-center rounded-full self-center w-6 h-6 mx-auto mb-1">
+                        <div className="text-[#7788f4] text-lg ">
+                          {" "}
+                          {data[expense.id - 1].icon}
+                        </div>
+                      </div>
+                      <p className="font-light text-xs text-center">
+                        {expense.name}
+                      </p>
                     </div>
                     <p className="font-light text-xs text-center">
-                      {expense.name}
+                      ${expense.amount}
                     </p>
+                    <div className="flex gap-2">
+                      <HiOutlinePencil
+                        className="text-[#a7a7a7] cursor-pointer"
+                        onClick={() => {
+                          handleclick(expense.id);
+                          dispatch(setMainExpense(expense.amount));
+                          dispatch(setCurrentCategory(expense.name));
+                          dispatch(setIsEditing(true));
+                          dispatch(setEditID(expense.id));
+                        }}
+                      />
+                      <RiDeleteBin5Line
+                        className="text-[#7788f4] cursor-pointer"
+                        onClick={() => dispatch(removeExpenseItem(expense.id))}
+                      />
+                    </div>
                   </div>
-                  <p className="font-light text-xs text-center">
-                    ${expense.amount}
-                  </p>
-                  <div className="flex gap-2">
-                    <HiOutlinePencil
-                      className="text-[#a7a7a7] cursor-pointer"
-                      onClick={() => {
-                        handleclick(expense.id);
-                        dispatch(setMainExpense(expense.amount));
-                        dispatch(setCurrentCategory(expense.name));
-                        dispatch(setIsEditing(true));
-                        dispatch(setEditID(expense.id));
-                      }}
-                    />
-                    <RiDeleteBin5Line
-                      className="text-[#7788f4] cursor-pointer"
-                      onClick={() => dispatch(removeExpenseItem(expense.id))}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
